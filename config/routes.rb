c18151f6c3d5ecbeb7eb1a8114919e0cf0984b7c
Ryan Yogan <ryanyogan@gmail.com>
2014-03-29 00:57:30 -0500

    Basic configuration of angular skeleton

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Angularrails::Application.routes.draw do
+  get "hello_angular/show"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
